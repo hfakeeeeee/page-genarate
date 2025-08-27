6ce68394450f5427951cf328e289085b8f1fc096
Nguyen Quoc Huy <nqhuy204@clc.fitus.edu.vn>
2025-08-27 23:35:24 +0700

    Init

--- dhp-ai-web-builder-agent/app/main/configs/MainConfig.py
+++ dhp-ai-web-builder-agent/app/main/configs/MainConfig.py
@@ -3,5 +3,5 @@ import dotenv
 
 dotenv.load_dotenv()
 
-SA_CONNECTION = os.environ.get("SA_CONNECTION") or "DefaultEndpointsProtocol=https;AccountName=dhp0search0npe;AccountKey=vOdji7Zrhe/3HAUcdTkhClWie9+e8JM6HPyGmXXxGGtEAMvtMiIlgL0HlAfOYPheLxyzMv8qTUU/+AStfhtoYw==;EndpointSuffix=core.windows.net"
+SA_CONNECTION = os.environ.get("SA_CONNECTION") 
 SA_SHARE_NAME = os.environ.get("SA_SHARE_NAME") or "file-search-image-dev"
